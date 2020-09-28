d011610e1f072f3edaa5dbf7796d8eb4f6a3c2e0
denysLerroan <lerroandeco@gmail.com>
2020-09-28 19:25:58 -0300

    For Example

---
+++ Python Code/For_example.py
@@ -0,0 +1,17 @@
+def Soma(a):
+    r = 0
+    for i in range(a):
+        r += i
+    return r
+
+s = Soma(5)
+print(s)
+
+for i in range(6):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(6,0,-1):
+    print(i)
